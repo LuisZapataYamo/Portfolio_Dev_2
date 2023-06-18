e8b9a6bc649c3eb672c8b8247d6b5e63fc33f2be
LuisZapataYamo <jhonayquerdev@gmail.com>
2023-06-18 00:23:35 -0500

    title and formatting button component

--- src/components/utils/Button/Button.jsx
+++ src/components/utils/Button/Button.jsx
@@ -1,12 +1,30 @@
 import "./Button.css"
-
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Button = ({ text, icon, to, ph = '0', pv = '0', h = '0', w = '0', bgcolor = "var(--yellow)", txcolor = "var(--black)", fsize = '1.5rem' }) => {
+const Button = ({
+    text,
+    icon,
+    to,
+    ph = '0',
+    pv = '0',
+    h = '0',
+    w = '0',
+    bgcolor = "var(--yellow)",
+    txcolor = "var(--black)",
+    fsize = '1.5rem'
+}) => {
     return (
         <Link to={to} className="button-component">
-            <div style={{ paddingLeft: ph, paddingRight: ph, paddingTop: pv, paddingBottom: pv, height: h, width: w, backgroundColor: bgcolor, color: txcolor }}>
+            <div style={{
+                paddingLeft: ph,
+                paddingRight: ph,
+                paddingTop: pv,
+                paddingBottom: pv,
+                height: h,
+                width: w,
+                backgroundColor: bgcolor,
+                color: txcolor
+            }}>
                 <span className="text" style={{ fontSize: fsize }}>{text}</span>
                 {icon && { icon }} {/* Utilicé una etiqueta img para mostrar el icono */}
             </div>
