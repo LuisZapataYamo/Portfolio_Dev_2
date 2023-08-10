7988d644179778dbba941d7b399ee2b776fbe2dc
LuisZapataYamo <jhonayquerdev@gmail.com>
2023-08-10 10:32:35 -0500

    Add new path in route

--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -2,6 +2,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../components/Home/Home.jsx";
 import GlobalStateContext from "../context/GlobalStateContext.jsx";
+import Skills from "../components/Skills/Skills.jsx";
+import About from "../components/About/About.jsx";
 
 const Routing = () => {
     return (
@@ -11,6 +13,8 @@ const Routing = () => {
                     <Routes>
                         <Route element={<App/>}>
                             <Route index element={<Home/>}/>
+                            <Route path='/about/' element={<About/>} />
+                            <Route path='/skills/' element={<Skills/>} />
                         </Route>
 
 
